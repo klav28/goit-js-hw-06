af85926303dcd69de4205e27e9f18573b995946e
klav28 <klav28@gmail.com>
2023-02-28 01:43:33 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const fontSizeControl = document.querySelector("#font-size-control");
 const textField = document.querySelector("#text");
 
 const handleSizeControl = () => {
-    textField.style.fontSize = fontSizeControl.value + "px";
+    textField.style.fontSize = `${fontSizeControl.value}px`;
 }
 
 fontSizeControl.addEventListener("input", handleSizeControl);
