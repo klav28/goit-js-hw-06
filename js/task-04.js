9b0c5dad7f21f5a2923a50fa00480b7a88af0b22
klav28 <klav28@gmail.com>
2023-02-28 11:15:56 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,12 +1,25 @@
 let counterValue = 0;
 
-const buttonEl = document.querySelectorAll("button");
 const counterTextEl = document.querySelector("#value");
+// const buttonEl = document.querySelectorAll("button");
 
+// const handleClick = event => {
+//     if (event.target.dataset.action === "increment") counterValue += 1;
+//     if (event.target.dataset.action === "decrement") counterValue -= 1;
+//     counterTextEl.textContent = counterValue;
+// }
+// buttonEl.forEach(el => el.addEventListener("click", handleClick));
+
+const counterForm = document.querySelector("#counter");
 const handleClick = event => {
-    if (event.target.dataset.action === "increment") counterValue += 1;
-    if (event.target.dataset.action === "decrement") counterValue -= 1;
+    switch (event.target.dataset.action) {
+        case "increment":
+            counterValue += 1;
+            break;
+        case "decrement": 
+            counterValue -= 1;
+    }
     counterTextEl.textContent = counterValue;
 }
 
-buttonEl.forEach(el => el.addEventListener("click", handleClick));
+counterForm.addEventListener("click", handleClick);
